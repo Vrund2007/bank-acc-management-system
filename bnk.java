e1f9242919c802e40ccc2daff9c8a1083fc9168f
Vrund2007 <vrundjotania20@gmail.com>
2024-12-28 19:27:03 +0530

    Fixing bugs in method case7

--- bnk.java
+++ bnk.java
@@ -4,17 +4,18 @@ class Acc{
     Scanner sc = new Scanner(System.in);
 
     void set(){
-        System.out.println("Enter Account Number");
+        System.out.print("Enter Account Number : ");
         int no = sc.nextInt();
         sc.nextLine(); //Flush
-        System.out.println("Enter Account Owner's name");
+        System.out.print("Enter Account Owner's name : ");
         String name = sc.nextLine();
-        System.out.println("Enter Account Balance");
+        System.out.print("Enter Account Balance : ");
         int bal = sc.nextInt();
 
         ano = no;
         abal = bal;
         aname = name;
+		System.out.println();
     }
 
     void get(){
@@ -22,10 +23,11 @@ class Acc{
         System.out.println("Account Owner's Name : "+aname);
         System.out.println("Account Balance : "+abal);
         System.out.println("Bank Name : " + bankname);
+		System.out.println();
     }
 
     void case1(Acc [] a){
-       System.out.println("Enter account number : "); 
+       System.out.print("Enter account number : "); 
        int sno = sc.nextInt();
        boolean check = false;
        
@@ -42,7 +44,7 @@ class Acc{
     }
 
     void case2(Acc [] a){
-        System.out.println("Enter Balance : ");
+        System.out.print("Enter Balance : ");
         int sbal = sc.nextInt();
         boolean check = false;
 
@@ -59,7 +61,7 @@ class Acc{
     }
 
     void case3(Acc [] a){
-        System.out.println("Enter Account owner's name : ");
+        System.out.print("Enter Account owner's name : ");
         String sname = sc.nextLine();
         boolean check = false;
        
@@ -76,7 +78,7 @@ class Acc{
     }
 
     void case4(Acc [] a){
-        System.out.println("Enter First char of Account owner/s");
+        System.out.print("Enter First char of Account owner/s : ");
         char schar = sc.next().toUpperCase().charAt(0);
         boolean check = false;
        
@@ -116,7 +118,7 @@ class Acc{
     void case6(Acc [] a){
         System.out.print("Enter Account Number : ");
         int sno = sc.nextInt();
-        System.out.print("Enter Amount to be deposit : ");
+        System.out.print("Enter Amount to withdraw : ");
         int wit= sc.nextInt();
         boolean check = false;
 
@@ -138,26 +140,26 @@ class Acc{
         System.out.println();
     }
 
-    void case7(Acc [] a){
-        int [] balance = new int[a.length];
-        for(int i = 0; i<a.length; i++){
-            balance [i] = a[i].abal;
+    void case7(Acc[] a) {
+        int[] balance = new int[a.length]; // Created balance array for storing the array
+        for (int i = 0; i < a.length; i++) {
+            balance[i]=a[i].abal;
         }
-        Arrays.sort(balance);
-
-        for(int i = balance.length - 1; i>balance.length-4; i--){
-            if(a[i].abal == balance[i]){
+        Arrays.sort(balance); // Balance stored in the array
+        for (int i = a.length-1; i > a.length-4; i--) {
+            if(a[i].abal==balance[i]) {
                 a[i].get();
+                System.out.println();
             }
         }
-    }
+	}
 
     void case8(Acc [] a){
-        System.out.println("Enter new Bank Name : ");
+        System.out.print("Enter new Bank Name : ");
         sc.nextLine(); // Flush
         String newBankName = sc.nextLine();
         bankname = newBankName;
-        System.out.println("Bank Name updated successfully to: " + bankname);
+        System.out.println("Bank Name updated successfully to : " + bankname);
         System.out.println();
     }
 }
@@ -166,12 +168,13 @@ class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the Bank Name: ");
+        System.out.print("Enter the Bank Name : ");
         Acc.bankname = sc.nextLine();
 
-        System.out.print("Enter no of Account in Bank :");
+        System.out.print("Enter no of Account in Bank : ");
         int n = sc.nextInt();
-
+		System.out.println();
+		
         //Making array for all Account
         Acc [] a = new Acc [n];
         Acc m = new Acc();
@@ -180,6 +183,7 @@ class Main{
         for(int i = 0; i<n ; i++ ){
             a[i] = new Acc();
             a[i].set();
+			System.out.println();
         }
 
         while (true) {
@@ -192,11 +196,11 @@ class Main{
             System.out.println("Enter 7 to view details of the top three accounts with the highest balances.");
             System.out.println("Enter 8 to update the bank's name for all accounts.");
             System.out.println("Enter 9 to exit the application.");
-
-            System.out.println();
-
-            System.out.println("Enter Choice : ");
+			System.out.println();
+			
+            System.out.print("Enter Choice: ");
             int ch = sc.nextInt();
+			System.out.println();
 
             switch (ch) 
             {
